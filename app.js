b56dad70bd9203a04d283833670b11ace88dc58b
Nicholas Oh <nicholasohjj@Nicholass-MacBook.local>
2026-04-24 11:13:41 +0800

    Use cp2nus

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.use("/", cp2);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, "127.0.0.1", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`App listening on port: ${port}`);
 });
